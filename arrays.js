c0c45ee407c2709e963e8d8bdd3f3e2477e4369e
jzlbornoz <javieraap10@gmail.com>
2022-08-23 11:28:05 -0400

    [array]Delete and shiftIndex

--- arrays.js
+++ arrays.js
@@ -13,6 +13,38 @@ class myArray {
         this.length++
         return this.data
     }
+    pop() {
+        const lastItem = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+        return lastItem;
+    }
+    delete(index) {
+        const item = this.data[index];
+        this.shiftIndex(index);
+        return item;
+    }
+
+    //Genera el cambio del indice posterior al eliminado del elemento
+    shiftIndex(index) {
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        delete this.data[this.length - 1];
+        this.length--;
+    }
 }
 
 const array = new myArray();
+
+
+array.push("Juan");
+array.push("Jose");
+array.push("Ana");
+
+console.log(array);
+//console.log("Deleted: " + array.pop());
+console.log("==============================");
+console.log(array);
+console.log(array.delete(0));
+console.log(array);
