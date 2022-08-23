914f888678c4312d9c5f34d22ed16645d07872a2
jzlbornoz <javieraap10@gmail.com>
2022-08-23 19:27:10 -0400

    Implementacion del metodo get

--- hashTable.js
+++ hashTable.js
@@ -14,6 +14,19 @@ class HashTable {
         this.data[address].push([key, value]);
         return this.data;
     }
+    get(key) {
+        const address = this.hashMethod(key);
+        const currentBucket = this.data[address]; // currentBucket es como decir el indice en un array
+        if (currentBucket) {
+            for (let i = 0; i < currentBucket.length; i++) {
+                if (currentBucket[i][0] === key) {
+                  return currentBucket[i][1];
+                }
+
+            }
+        }
+        return undefined;
+    }
 
 }
 const myHashTable = new HashTable(50);
@@ -25,3 +38,4 @@ myHashTable.set("Pedro", 1);
 myHashTable.set("Eva", 14)
 myHashTable.set("Anda", 15);
 console.log(myHashTable)
+console.log(myHashTable.get("Eva"));
