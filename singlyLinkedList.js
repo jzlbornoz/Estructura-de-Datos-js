535348457bc407249f6fab60081379df5276eff3
jzlbornoz <javieraap10@gmail.com>
2022-08-30 10:29:22 -0400

    [linked list] metodo Insert y Delete

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -49,6 +49,34 @@ class MySyngleLinkedList {
         this.length++;
         return this;
     }
+    insert(index, value) {
+        if (index >= this.length) {
+            return this.append(value);
+        }
+        const node = new Node(value);
+        const firstPointer = this.getIndex(index - 1);
+        const holdingPointer = firstPointer.next;
+        firstPointer.next = node;
+        node.next = holdingPointer;
+        this.length++
+        return this
+    }
+    remove(index) {
+        const previousPointer = this.getIndex(index - 1);
+        const holdingPointer = this.getIndex(index + 1);
+        previousPointer.next = holdingPointer;
+        this.length--;
+        return this;
+    }
+    getIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+        while (counter !== index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode;
+    }
 }
 
 let LinkedList = new MySyngleLinkedList(1);
@@ -56,4 +84,6 @@ let LinkedList = new MySyngleLinkedList(1);
 console.log(LinkedList);
 console.log(LinkedList.append(23));
 console.log(LinkedList);
-console.log(LinkedList.prepend(12));
+console.log(LinkedList.append(12));
+console.log(LinkedList.insert(1, 34));
+console.log(LinkedList.remove(1));
